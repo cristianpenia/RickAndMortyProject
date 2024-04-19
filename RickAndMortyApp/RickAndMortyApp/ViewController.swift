690195c1aa80a2fd4315babfbaeb47ed2e9812d9
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-04-19 12:35:50 -0600

    initial commit

---
+++ RickAndMortyApp/RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Cristian Peña Barrios on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
