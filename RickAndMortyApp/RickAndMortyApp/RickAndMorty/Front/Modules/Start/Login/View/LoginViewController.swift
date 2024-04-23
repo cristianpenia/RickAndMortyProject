a160cc4af5f9920a90f677b4435e082079615942
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-04-23 10:34:21 -0600

    [ADD] Login view

--- RickAndMortyApp/RickAndMortyApp/RickAndMorty/Front/Modules/Start/Login/View/LoginViewController.swift
+++ RickAndMortyApp/RickAndMortyApp/RickAndMorty/Front/Modules/Start/Login/View/LoginViewController.swift
@@ -8,19 +8,55 @@
 
 import UIKit
 
-class LoginViewController: BaseViewController, LoginViewInput {
-
+class LoginViewController: BaseViewController {
+    
+    
+    // MARK: Views
+    
+    @IBOutlet weak var informationButton: UIButton!
+    
+    @IBOutlet weak var appImageView: UIImageView!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
+    // MARK: Properties
+    
     var output: LoginViewOutput!
 
+    
     // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         output.viewIsReady()
     }
+    
+    
+    // MARK: Private methods
+    
+    
+    // MARK: Actions
+    
+    @IBAction func informationButtonAction(_ sender: Any) {
+        output.didTapInformationButton()
+    }
+    
+    @IBAction func sendButtonAction(_ sender: Any) {
+        output.didTapSendButton()
+    }
+}
 
 
-    // MARK: LoginViewInput
+// MARK: LoginViewInput
+
+extension LoginViewController: LoginViewInput {
+    
     func setupInitialState() {
     }
 }
